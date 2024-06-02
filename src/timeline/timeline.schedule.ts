55a8207037284662eac23f13335edb437888db28
DMQQ <kozakdamian73@gmail.com>
2024-06-02 14:31:35 +0200

    FIX: improved getting events in cron job & created query to get diff of monthly expenses

--- src/timeline/timeline.schedule.ts
+++ src/timeline/timeline.schedule.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Cron, Interval } from '@nestjs/schedule';
-import { TimelineService } from './timeline.service';
-import * as moment from 'moment';
+import { Cron } from '@nestjs/schedule';
 import { NotificationsService } from 'src/notifications/notifications.service';
-import { ExpoPushToken, ExpoPushMessage } from 'expo-server-sdk';
+import { ExpoPushMessage } from 'expo-server-sdk';
 import { TimelineScheduleService } from './timelineSchedule.service';
 
 @Injectable()
@@ -12,67 +10,56 @@ export class TimelineSchedule {
     private notifiService: NotificationsService,
     private timelineScheduleService: TimelineScheduleService,
   ) {}
+
   @Cron('0 * * * * *', { timeZone: 'Europe/Warsaw' })
   async handleCron() {
-    const date = moment().format('YYYY-MM-DD');
-
-    const time = moment().format('HH:mm:ss');
-
-    const events = await this.timelineScheduleService.findEventsWithDateAndTime(
-      date,
-      time,
-    );
+    const events =
+      await this.timelineScheduleService.findEventsByTypeWithCurrentTime(
+        'beginTime',
+      );
 
     const messages = [] as ExpoPushMessage[];
 
     for (const event of events) {
-      const user = await this.notifiService.findUserToken(event.userId);
-
-      if (user) {
-        messages.push({
-          badge: 1,
-          to: user.token,
-          subtitle: 'Daily reminder',
-          sound: 'default',
-          title: event.title,
-          body: event.description,
-          data: { data: "Hello it's your daily reminder" },
-        });
-      }
+      messages.push({
+        badge: 1,
+        to: event.token,
+        subtitle: 'Daily reminder',
+        sound: 'default',
+        title: event.title.substring(0, 50) + '...',
+        body: event.description.substring(0, 50) + '...',
+        data: {
+          data: 'Your schedulded event is now running!',
+          eventId: event.id,
+        },
+      });
     }
 
-    const output = await this.notifiService.sendChunkNotifications(messages);
+    await this.notifiService.sendChunkNotifications(messages);
   }
 
   @Cron('0 * * * * *', { timeZone: 'Europe/Warsaw' })
   async handleEndingEvents() {
-    const currentDate = moment().format('YYYY-MM-DD');
-
-    const currentTime = [...moment().format('HH:mm').split(':'), '00'].join(
-      ':',
-    );
-
-    const events = await this.timelineScheduleService.findEndingEvents(
-      currentDate,
-      currentTime,
-    );
+    const events =
+      await this.timelineScheduleService.findEventsByTypeWithCurrentTime(
+        'endTime',
+      );
 
     const messages = [] as ExpoPushMessage[];
 
     for (const event of events) {
-      const user = await this.notifiService.findUserToken(event.userId);
-
-      if (user) {
-        messages.push({
-          badge: 1,
-          to: user.token,
-          subtitle: 'Your event is ending',
-          sound: 'default',
-          title: event.title + 'ends at' + event.endTime,
-          body: event.description,
-          data: { data: "Hello it's your daily reminder" },
-        });
-      }
+      messages.push({
+        badge: 1,
+        to: event.token,
+        subtitle: 'Daily reminder',
+        sound: 'default',
+        title: event.title.substring(0, 50) + '...',
+        body: event.description.substring(0, 50) + '...',
+        data: {
+          data: 'Your schedulded event is now running!',
+          eventId: event.id,
+        },
+      });
     }
 
     await this.notifiService.sendChunkNotifications(messages);
