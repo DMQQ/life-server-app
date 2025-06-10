4d8eaff8d6d5339a24cde698db51afeef2f1870c
DMQQ <kozakdamian73@gmail.com>
2025-06-10 12:08:43 +0200

    Upgrade nestjs to version 11 from 8, add compression to requests

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import * as compression from 'compression';
 
-const ADDR = '192.168.1.20';
+const ADDR = '10.1.1.105';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
+  app.use(
+    compression({
+      level: 9,
+      threshold: 0,
+      memLevel: 9,
+      chunkSize: 16384,
+    }),
+  );
   if (process.env.NODE_ENV === 'development') {
     await app.listen(process.env.APP_PORT || 3000, ADDR, () => {
       console.log(`Server is running on http://${ADDR}:${process.env.APP_PORT}/`);
