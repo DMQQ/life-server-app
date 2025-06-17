6a245a2d513f368310e333af53096c3e9a4ad2dc
DMQQ <kozakdamian73@gmail.com>
2025-06-17 12:40:50 +0200

    Fix goals reminder module setup

--- src/goals/goals.module.ts
+++ src/goals/goals.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GoalCategory, GoalEntry, UserGoal } from './goals.entity';
 import { GoalResolver } from './goals.resolver';
 import { GoalReminderScheduler } from './crons/goal-reminder.scheduler';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserGoal, GoalCategory, GoalEntry])],
+  imports: [TypeOrmModule.forFeature([UserGoal, GoalCategory, GoalEntry]), NotificationsModule],
   providers: [GoalService, GoalResolver, GoalReminderScheduler],
 })
 export class GoalsModule {}
